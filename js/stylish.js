178fee3edd748abc4e0100d730503be9a61ab7dc
rtdeedqgryet <borismariondorier@gmail.com>
2020-09-24 21:09:04 +0200

    caca

---
+++ js/stylish.js
@@ -0,0 +1,27 @@
+let c = document.getElementById("container");
+let ca = document.getElementById("carroussel")
+
+
+async function turn(){
+  for(let i = 0; i < c.childElementCount; i++){
+    
+    await sleep(2000);
+    console.log(i + "childEle : " + c.childElementCount);
+    c.style.transform = `translateX(-${i*window.innerWidth}px)`;
+    if(i == c.childElementCount-1){
+      c.style.transitionDuration = "0s";
+      console.log("last");
+      
+    }
+    if(i == 1){
+      c.style.transitionDuration = "0.5s";
+    }
+    
+
+  }
+  turn();
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
